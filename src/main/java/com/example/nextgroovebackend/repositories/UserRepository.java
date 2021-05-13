f9577fe6070eed6264a74c7bd3d68b1300c67c84
Jack Curtin <jackcurtin94@gmail.com>
2021-05-13 15:19:20 -0500

    created UserProfileRepository

--- src/main/java/com/example/nextgroovebackend/repositories/UserRepository.java
+++ src/main/java/com/example/nextgroovebackend/repositories/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.nextgroovebackend.repositories;
 
-public interface UserRepository {
+import com.example.nextgroovebackend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail (String email);
 }
--- src/main/java/com/example/nextgroovebackend/services/UserService.java
+++ src/main/java/com/example/nextgroovebackend/services/UserService.java
@@ -1,8 +1,10 @@
 package com.example.nextgroovebackend.services;
 
+import com.example.nextgroovebackend.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
 
 }
