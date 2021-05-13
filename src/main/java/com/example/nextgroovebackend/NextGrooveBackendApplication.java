6667384df81aa36fed16c97d878243e27df29f8f
Jack Curtin <jackcurtin94@gmail.com>
2021-05-13 14:39:21 -0500

    first commit - created spring project and created User Model

---
+++ src/main/java/com/example/nextgroovebackend/NextGrooveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextgroovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGrooveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextGrooveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nextgroovebackend/NextGrooveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextgroovebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGrooveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
