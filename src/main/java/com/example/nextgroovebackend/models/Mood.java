309bb13126c96e2d24803d7f4a3c93a7b98f9416
Jack Curtin <jackcurtin94@gmail.com>
2021-05-14 09:34:12 -0500

    mood model created

---
+++ src/main/java/com/example/nextgroovebackend/models/Mood.java
@@ -0,0 +1,72 @@
+package com.example.nextgroovebackend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
+public class Mood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column
+    private int fastSlowValue;
+    @Column
+    private int upbeatDepressingValue;
+
+    @OneToMany
+    @JsonIgnore
+    private List<Record> recordList;
+
+    public Mood() {
+    }
+
+    public Mood(int fastSlowValue, int upbeatDepressingValue) {
+        this.fastSlowValue = fastSlowValue;
+        this.upbeatDepressingValue = upbeatDepressingValue;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public int getFastSlowValue() {
+        return fastSlowValue;
+    }
+
+    public void setFastSlowValue(int fastSlowValue) {
+        this.fastSlowValue = fastSlowValue;
+    }
+
+    public int getUpbeatDepressingValue() {
+        return upbeatDepressingValue;
+    }
+
+    public void setUpbeatDepressingValue(int upbeatDepressingValue) {
+        this.upbeatDepressingValue = upbeatDepressingValue;
+    }
+
+    public List<Record> getRecordList() {
+        return recordList;
+    }
+
+    public void setRecordList(List<Record> recordList) {
+        this.recordList = recordList;
+    }
+
+    @Override
+    public String toString() {
+        return "Mood{" +
+                "Id=" + Id +
+                ", fastSlowValue=" + fastSlowValue +
+                ", upbeatDepressingValue=" + upbeatDepressingValue +
+                '}';
+    }
+}
