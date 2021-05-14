198c82f7b7716fc993f3f3e43b2b3b19733a49c9
Jack Curtin <jackcurtin94@gmail.com>
2021-05-14 08:54:02 -0500

    genre model created

--- src/main/java/com/example/nextgroovebackend/models/Genre.java
+++ src/main/java/com/example/nextgroovebackend/models/Genre.java
@@ -1,6 +1,9 @@
 package com.example.nextgroovebackend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "genres")
@@ -8,4 +11,42 @@ public class Genre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany
+    @JsonIgnore
+    private List<Record> recordList;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
