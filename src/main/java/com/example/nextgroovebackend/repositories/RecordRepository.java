01a0e439cc0e750f30cc7d2a85d6f52e3b3410e3
Jack Curtin <jackcurtin94@gmail.com>
2021-05-14 10:15:33 -0500

    createRecord method in complete

--- src/main/java/com/example/nextgroovebackend/repositories/RecordRepository.java
+++ src/main/java/com/example/nextgroovebackend/repositories/RecordRepository.java
@@ -4,6 +4,9 @@ import com.example.nextgroovebackend.models.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecordRepository extends JpaRepository <Record, Long> {
+    Optional<Record> findByTitleAndArtist(String recordTitle, String recordArtist);
 }
--- src/main/java/com/example/nextgroovebackend/services/RecordService.java
+++ src/main/java/com/example/nextgroovebackend/services/RecordService.java
@@ -1,8 +1,14 @@
 package com.example.nextgroovebackend.services;
 
+import com.example.nextgroovebackend.exceptions.InformationExistsException;
+import com.example.nextgroovebackend.models.Genre;
+import com.example.nextgroovebackend.models.Record;
 import com.example.nextgroovebackend.repositories.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class RecordService {
@@ -10,4 +16,25 @@ public class RecordService {
 
     @Autowired
     public void setRecordRepository (RecordRepository recordRepository) {this.recordRepository = recordRepository;}
+
+    public Record createRecord(Map<String, String> recordObject){
+        System.out.println("Record service is calling createRecord");
+        String recordTitle = recordObject.get("title");
+        String recordArtist = recordObject.get("artist");
+        Optional<Record> recordOptional = recordRepository.findByTitleAndArtist(recordTitle, recordArtist);
+        if(recordOptional.isPresent() &&
+                recordOptional.get().getTitle().equalsIgnoreCase(recordTitle) &&
+                recordOptional.get().getArtist().equalsIgnoreCase(recordArtist)){
+            throw new InformationExistsException("The following record already appears in our database: \n" +
+                    recordTitle + " -- " + recordArtist);
+        }
+        else{
+            Record newRecord = new Record();
+            return assembleOrUpdateRecord(newRecord, recordObject);
+        }
+    }
+
+    private Record assembleOrUpdateRecord(Record newRecord, Map <String, String> recordObject){
+        Optional<Genre> genreOptional =
+    }
 }
