fb27956d2eb6a0ada24441f5693552912f466eb8
Jack Curtin <jackcurtin94@gmail.com>
2021-05-19 10:43:34 -0500

    reverted

--- src/main/java/com/example/nextgroovebackend/security/SecurityConfigurer.java
+++ src/main/java/com/example/nextgroovebackend/security/SecurityConfigurer.java
@@ -32,9 +32,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                "/*"
-//                "/auth/register", "/auth/login", "/albums/browse/*",
-//        "/genres/*"
+                "/auth/register", "/auth/login", "/albums/browse/*",
+        "/genres/*"
         ).permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
