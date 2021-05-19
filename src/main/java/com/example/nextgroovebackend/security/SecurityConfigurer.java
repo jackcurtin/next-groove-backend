023c91d921485b9516e0e8cb50aedf677a661032
Jack Curtin <jackcurtin94@gmail.com>
2021-05-19 10:37:42 -0500

    made create profile private

--- src/main/java/com/example/nextgroovebackend/security/SecurityConfigurer.java
+++ src/main/java/com/example/nextgroovebackend/security/SecurityConfigurer.java
@@ -32,7 +32,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                "/auth/register", "/auth/login", "/auth/create-profile", "/albums/browse/*",
+                "/auth/register", "/auth/login", "/albums/browse/*",
         "/genres/*").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
